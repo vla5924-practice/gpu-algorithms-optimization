2e1bd7e0e2e8b771791808d25db233751dcea3f3
vla5924 <vla5924@gmail.com>
2021-08-22 19:47:35 +0300

    Added launch script generator instead of old .sh scripts

---
+++ benchmarking/scripts/common.sh
@@ -0,0 +1,17 @@
+# This script must be source`d from others in subdirectories
+
+dir=`dirname $0`
+repo=`realpath $dir/../..`
+datasets=$repo/datasets
+logs=$repo/logs
+iters=${1:-1}
+miniconda_dir=/home/$USER/huawei/miniconda3
+timestamp=`date "+%Y%m%d%H%M%S"`
+
+if [ -f "$miniconda_dir/etc/profile.d/conda.sh" ]; then
+    source "$miniconda_dir/etc/profile.d/conda.sh"
+else
+    export PATH="$miniconda_dir/bin:$PATH"
+fi
+
+conda activate rapids-0.18
---
+++ benchmarking/tools/generate_launch_script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+scripts_dir=$(realpath $(dirname $0)/../scripts)
+provider=$1
+target_dir="${scripts_dir}/${provider}"
+algorithm=$2
+format=${3:-npy}
+
+mkdir -p "${scripts_dir}/${provider}"
+
+echo "#!/bin/sh
+
+#SBATCH --time=4320
+
+set -e
+
+. \$(dirname \$0)/../common.sh
+
+python3 \$repo/run_benchmark.py \
+    -m bench.spark.kmeans \
+    -r \$repo \
+    -d \$datasets \
+    -f ${format} \
+    -i \$iters \
+    -l \$logs/${provider}-${algorithm}-\$timestamp.log \
+    -- -tf %D --double
+" > ${target_dir}/${algorithm}.sh
