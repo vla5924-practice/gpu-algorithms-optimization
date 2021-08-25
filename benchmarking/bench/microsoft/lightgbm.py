d35492c3e2aca3e88eaad24c84602a2eeef1cc64
vla5924 <vla5924@gmail.com>
2021-08-25 12:30:00 +0300

    lightgbm initial benchmark impl

---
+++ benchmarking/bench/microsoft/lightgbm.py
@@ -0,0 +1,55 @@
+import lightgbm
+import numpy as np
+
+from sklearn.metrics import accuracy_score
+
+from common import argparser, dataset, utils
+# from common import cuda
+from common.timer import Timer
+
+
+args = argparser.build_parser().parse_args()
+
+print("Fit dataset:", args.fit_file)
+
+dtype = np.float64 if args.double else np.float32
+print("Type elements:", dtype)
+
+X, y = dataset.open_auto_extract_dataset(args.fit_file, dtype)
+print("Fit targets:", y.shape)
+print("Fit samples:", X.shape)
+
+# common.cuda_warm_up()
+
+fit_dataset = lightgbm.Dataset(X, label=y)
+
+timer = Timer()
+params = {
+    "objective": "binary",
+    "metric": "auc",
+    "device": "cpu",
+    # "device": "gpu",
+    # "gpu_device_id": 0,
+}
+model = lightgbm.train(params, fit_dataset)
+print("Fit time:", timer.count())
+
+if (args.test):
+    print("Test dataset:", args.test_file)
+    X_t, y_t = dataset.open_auto_extract_dataset(args.test_file, dtype)
+    print("Test targets:", y_t.shape)
+    print("Test samples:", X_t.shape)
+
+    timer = Timer()
+    preds = model.predict(X_t)
+    print("Predicting time:", timer.count())
+
+    utils.print_params({
+        "Real": y_t,
+        "Predictions": preds,
+    }, space="\n")
+
+    utils.print_many("#")
+    utils.print_params({
+        "Accuracy score": accuracy_score(y_t, preds),
+    })
